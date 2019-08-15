e8aa7e507a59d056a894dee34268a7b1be8203a2
smessiah777 <smessiah17@gmail.com>
2019-08-15 14:52:53 -0500

    refactored App.js created context for alert methods and components

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,17 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
+import AlertContext from '../../context/alert/alertContext';
 import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ setAlert }) => {
+const Search = () => {
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
 
   const [text, setText] = useState('');
 
   const onSubmit = e => {
     e.preventDefault();
     if (text === '') {
-      setAlert('Please enter something', 'light');
+      alertContext.setAlert('Please enter something', 'light');
     } else {
       githubContext.searchUsers(text);
       setText('');
@@ -46,10 +47,6 @@ const Search = ({ setAlert }) => {
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
